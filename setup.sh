031c285ea5accbdc47f1428c68d7106ab77a10ee
Markus <1685937+airjump@users.noreply.github.com>
2019-08-01 12:50:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-apt-get -y install python-pip git python-dev
+docker run -it debian
+apt-get update
+apt-get upgrade
+apt-get install ansible git python-pip
+
 mkdir ~/src
 cd ~/src
 git clone https://github.com/airjump/play-with-docker.git setup
