dc02ae7d988d797491b5469bcfe22caba2ee9b84
Markus <1685937+airjump@users.noreply.github.com>
2020-02-21 12:15:13 +0100

    Delete parse-test.py

--- parse-test.py
+++
@@ -1,8 +0,0 @@
-from lxml import etree
-root = etree.parse("fruits.xml").getroot()
-
-for name in root.findall("item/name"):
-    etree.dump(name)
-
-for name in root.findall("item/count"):
-    etree.dump(count)
